c2286585e0ae660712ded3e1dcbddad6a773b9d3
Zszywacz <teresher935@GMail.com>
2020-01-03 16:05:43 +0100

    half of Lab2

--- Lab02/www2/app.js
+++ Lab02/www2/app.js
@@ -1,13 +1,20 @@
+//REST
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
 var integer = { value: 34 }
 
+var https = require('https')
+var fs = require('fs')
+var path = require('path')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, }))
 
-var whitelist = 'http://chowrat.org:80/'
+
+//pozwalamy tylko strona z whitelist
+var whitelist = 'https://chowrat.org/'
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -22,6 +29,7 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+//reczne ustawianie naglowka pozwalajacego
 /*app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://chowrat.org:80/');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -40,5 +48,12 @@ app.post('/', (req, res) => {
     res.json(integer)
 })
 
+https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'sec/chowrat.net.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'sec/chowrat.net.crt'))
+}, app).listen( port, function(){
+  console.log(`Example app listening on port ${port}!`)
+} )
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- Lab02/www2/router.js
+++ Lab02/www2/router.js
@@ -1,7 +1,14 @@
+//http
 var express = require('express')
 var app = express()
+var fs = require('fs')
+var https = require('https')
+var http = require('http')
 var router = express.Router()
 var path = require('path')
+var forceSsl = require('express-force-ssl');
+
+
 
 // simple logger for this router's requests
 // all requests to this router will first hit this middleware
@@ -12,17 +19,9 @@ router.use(function (req, res, next) {
 
 // this will only be invoked if the path starts with /bar from the mount point
 router.use('/', function (req, res, next) {
-  
-  /*res.send(path.join(__dirname, 'public'));*/
-
-
-
-  
-  //res.send('okej')
-  // ... maybe some additional /bar logging ...
   next()
 })
-router.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
   res.set('Content-Type', 'text/html')
   var options = {
     root: path.join(__dirname, 'public'),
@@ -33,10 +32,39 @@ router.get('/', function(req, res) {
     }
   }
   res.sendFile('index.html', options) 
-});
+});*/
+
+app.get('/', function(req, res){
+  res.set('Content-Type', 'text/html')
+  var options = {
+    root: path.join(__dirname, 'public'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+  res.sendFile('index.html', options)
+})
+
+app.get('/rest', function(req, res){
+})
+app.post('/rest', function(req, res){
+})
 
 
 //if would be /foo then chowrat.org:80/foo/home /etc
-app.use('/', router)
 
-app.listen(80)
+app.use('/', router)
+app.use(forceSsl)
+http.createServer(app).listen(80)
+
+https.createServer({
+  key: fs.readFileSync(path.join(__dirname,'sec/chowrat.org.key')),
+  cert: fs.readFileSync(path.join(__dirname,'sec/chowrat.org.crt'))
+}, app)
+.listen(443, function(){
+  console.log('Port 443 started')
+})
+
+
